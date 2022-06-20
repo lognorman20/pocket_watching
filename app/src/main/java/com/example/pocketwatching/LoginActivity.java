1b1d2676bfd4313d9f422873126fff55ea353c62
Logan Norman <logannormanss@gmail.com>
2022-06-20 13:51:35 -0700

    started building login page

---
+++ app/src/main/java/com/example/pocketwatching/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.example.pocketwatching;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText etUsername;
+    private EditText etPassword;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+        if (ParseUser.getCurrentuser() != null){
+            goMainActivity();
+        }
+
+        etUsername = findViewById(R.id.etUsername);
+        etPassword = findViewById(R.id.etPassword);
+
+
+    }
+}
