fc51fd6022d5ad44429e63cd00f9d1b87b2a297e
Logan Norman <logannormanss@gmail.com>
2022-06-21 10:11:18 -0700

    testing branches

--- app/src/main/java/com/example/pocketwatching/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/pocketwatching/Activities/ProfileActivity.java
@@ -17,6 +17,6 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         tvTest = findViewById(R.id.tvTest);
-        tvTest.setText("cheese");
+        tvTest.setText("butter");
     }
 }
