69932fa764f22735e653675b7e1d908319bc2bd8
Logan Norman <logannormanss@gmail.com>
2022-06-23 09:52:53 -0700

    init new branch

--- app/src/main/java/com/example/pocketwatching/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/pocketwatching/Activities/ProfileActivity.java
@@ -112,5 +112,6 @@ public class ProfileActivity extends AppCompatActivity {
         ethBalance.setText(userEthWallets.get(0).getEth().getBalance().toString());
         txCount.setText(userEthWallets.get(0).getCountTxs().toString());
         ethDiff.setText(userEthWallets.get(0).getEth().getPrice().getRate().toString());
+
     }
 }
