3dcdd7e0d1197fd6cd120136fdea0c031363e992
Logan Norman <logannormanss@gmail.com>
2022-07-06 10:17:33 -0700

    init android charts graph

--- app/src/main/java/com/example/pocketwatching/Activities/BalanceActivity.java
+++ app/src/main/java/com/example/pocketwatching/Activities/BalanceActivity.java
@@ -1,31 +1,43 @@
 package com.example.pocketwatching.Activities;
 
 import android.graphics.Color;
-import android.graphics.DashPathEffect;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 
 import com.example.pocketwatching.R;
 
+import java.util.ArrayList;
+
 import im.dacer.androidcharts.LineView;
 
+
 public class BalanceActivity extends AppCompatActivity {
-    private LineView lineView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.historical_balance_graph);
 
-
-
         LineView lineView = (LineView)findViewById(R.id.line_view);
-        lineView.setDrawDotLine(false); //optional
-        lineView.setShowPopup(LineView.SHOW_POPUPS_MAXMIN_ONLY); //optional
-        lineView.setBottomTextList(strList);
-        lineView.setColorArray(new int[]{Color.BLACK,Color.GREEN,Color.GRAY,Color.CYAN});
-        lineView.setDataList(dataLists); //or lineView.setFloatDataList(floatDataLists)
+
+        // init x axis labels
+        ArrayList<String> labels = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            labels.add(String.valueOf(i));
+        }
+
+        lineView.setBottomTextList(labels);
+        lineView.setColorArray(new int[]{
+                Color.BLACK,Color.GREEN,Color.GRAY,Color.CYAN});
+
+       ArrayList<Integer> data = new ArrayList<>();
+       for (int i = 0; i < 5; i++) {
+           data.add(i * 5);
+       }
+
+       ArrayList<ArrayList<Integer>> dataList = new ArrayList<>();
+       dataList.add(data);
+
+        lineView.setDataList(dataList); //or lineView.setFloatDataList(floatDataLists)
     }
 }
