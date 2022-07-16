885f444b41919a2690b7c7152b40fd9941bde7d7
Logan Norman <logannormanss@gmail.com>
2022-07-16 10:52:44 -0700

    Revert "remove etc"
    This reverts commit 832a54df1d6b93392be190a0d4c30488fa091efc.

---
+++ app/src/main/java/com/example/pocketwatching/Etc/ClaimsXAxisValueFormatter.java
@@ -0,0 +1,35 @@
+package com.example.pocketwatching.Etc;
+
+import android.util.Log;
+
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.formatter.ValueFormatter;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.TimeZone;
+
+public class ClaimsXAxisValueFormatter extends ValueFormatter {
+
+    List<Float> datesList;
+
+    public ClaimsXAxisValueFormatter(List<Float> arrayOfDates) {
+        this.datesList = arrayOfDates;
+    }
+
+    @Override
+    public String getAxisLabel(float value, AxisBase axis) {
+        Long unixTime = (long) (value * 1000);
+        return toDate(unixTime);
+    }
+
+    public static String toDate(long unixTime) {
+        Date date = new Date();
+        date.setTime(unixTime * 1000);
+        SimpleDateFormat sdf = new java.text.SimpleDateFormat("MMM dd");
+        TimeZone tz = TimeZone.getDefault();
+        sdf.setTimeZone(java.util.TimeZone.getTimeZone(String.valueOf(tz)));
+        return sdf.format(date);
+    }
+}
---
+++ app/src/main/java/com/example/pocketwatching/Etc/TokenAmountComparator.java
@@ -0,0 +1,17 @@
+package com.example.pocketwatching.Etc;
+
+import com.example.pocketwatching.Models.Ethplorer.PortfolioValues.Token;
+
+import java.util.Comparator;
+
+public class TokenAmountComparator implements Comparator<Token> {
+    @Override
+    public int compare(Token one, Token two) {
+        if (one.getTokenBalance() < two.getTokenBalance()){
+            return -1;
+        } else if (one.getTokenBalance() > two.getTokenBalance()) {
+            return 1;
+        }
+        return 0;
+    }
+}
