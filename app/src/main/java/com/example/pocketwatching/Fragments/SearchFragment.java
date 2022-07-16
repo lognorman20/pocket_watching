16a720b6bbd464829a37aca248dea485c1941693
Logan Norman <logannormanss@gmail.com>
2022-07-16 12:30:55 -0700

    rework search page ui

--- app/src/main/java/com/example/pocketwatching/Fragments/SearchFragment.java
+++ app/src/main/java/com/example/pocketwatching/Fragments/SearchFragment.java
@@ -62,6 +62,13 @@ public class SearchFragment extends Fragment {
         rvUsers.setLayoutManager(new LinearLayoutManager(getContext()));
         rvUsers.setAdapter(adapter);
 
+        searchView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchView.setIconified(false);
+            }
+        });
+
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
